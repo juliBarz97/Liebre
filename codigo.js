44c6692411197d9860dd38669779c127c8e6833e
julian <julianbarz97@gmail.com>
2021-12-21 20:24:50 -0300

    fix views2

--- codigo.js
+++ codigo.js
@@ -10,6 +10,15 @@ app.get('/', function(req, res){
     res.sendFile(path.resolve(__dirname, './views/codigo.html'));
 })
 
+app.get('/', function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
+
+app.get('/', function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+
+
 
 
 app.use('/public/', express.static(__dirname + '/public/')) //con este me toma las imagenes
